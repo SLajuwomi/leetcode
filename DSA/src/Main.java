682eaccaa97d6ed05058281ecc3925a3cb66fbac
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-06 15:45:38 -0500

    O(2n) + O(n^2)

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -6,10 +6,25 @@ public class Main {
     for (int i = 0; i < n; i++) {
       System.out.println(i);
     }
+
+    // Technically O(2n) but we drop constants for simplification
+    for (int j = 0; j < n; j++) {
+      System.out.println(j);
+    }
+  }
+
+  // O(n^2)
+  public static void doubleItems(int n) {
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.println(i + " " + j);
+      }
+    }
   }
 
   public static void main(String[] args) {
-    printItems(10);
+    // printItems(10);
+    doubleItems(10);
   }
 
 }

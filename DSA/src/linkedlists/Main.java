56ea90b6f82faa360809252c9e9e147906de0d80
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-10 22:28:16 -0500

    Prepend method

--- DSA/src/linkedlists/Main.java
+++ DSA/src/linkedlists/Main.java
@@ -3,12 +3,12 @@ package DSA.src.linkedlists;
 public class Main {
   public static void main(String[] args) {
 
-    LinkedList myLinkedList = new LinkedList(1);
-    myLinkedList.append(2);
+    LinkedList myLinkedList = new LinkedList(2);
+    myLinkedList.append(3);
 
-    System.out.println(myLinkedList.removeLast().value);
-    System.out.println(myLinkedList.removeLast().value);
-    System.out.println(myLinkedList.removeLast());
+    myLinkedList.prepend(1);
+
+    myLinkedList.printList();
 
   }
 }

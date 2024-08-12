51bd5616e8b448ba406ebfe9da048b38752c61f7
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-11 19:51:07 -0500

    Set index to value

--- DSA/src/linkedlists/Main.java
+++ DSA/src/linkedlists/Main.java
@@ -3,12 +3,11 @@ package DSA.src.linkedlists;
 public class Main {
   public static void main(String[] args) {
 
-    LinkedList myLinkedList = new LinkedList(0);
-    myLinkedList.append(1);
-    myLinkedList.append(2);
-    myLinkedList.append(3);
+    LinkedList myLinkedList = new LinkedList(11);
+    myLinkedList.append(23);
+    myLinkedList.append(7);
 
-    System.out.println(myLinkedList.get(2).value + "\n");
+    System.out.println(myLinkedList.set(5, 12));
 
     myLinkedList.printList();
 

93a05600ac23df7182912e482cd09a117275cfcd
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-14 22:16:36 -0500

    hasLoop Method (Leetcode problem) Linked Lists

--- DSA/src/linkedlists/LinkedList.java
+++ DSA/src/linkedlists/LinkedList.java
@@ -187,4 +187,17 @@ public class LinkedList {
       return slow.next;
     return slow;
   }
+
+  public boolean hasLoop() {
+    Node slow = head;
+    Node fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (fast == slow)
+        return true;
+    }
+
+    return false;
+  }
 }
